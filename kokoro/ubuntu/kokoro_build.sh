c7932a40774ef907c3595110f59b30c9b9e45bb3
Omer Sheikh <ojsheikh@gmail.com>
2022-06-22 12:53:19 -0400

    Fix repo cd path

--- kokoro/ubuntu/kokoro_build.sh
+++ kokoro/ubuntu/kokoro_build.sh
@@ -9,5 +9,5 @@ set -x
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/git.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd ${KOKORO_ARTIFACTS_DIR}/git/kokoro-codelab-ojsheikh
+cd ${KOKORO_ARTIFACTS_DIR}/git/kokoro-github-ojsheikh
 ./build.sh
