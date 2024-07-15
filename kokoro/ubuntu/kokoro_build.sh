395571fbdc67000647690a20c583c07d422795a7
Omer Sheikh <ojsheikh@gmail.com>
2024-07-15 16:41:00 -0400

    Update kokoro_build.sh

--- kokoro/ubuntu/kokoro_build.sh
+++ kokoro/ubuntu/kokoro_build.sh
@@ -9,5 +9,5 @@ set -x
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/git.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd ${KOKORO_ARTIFACTS_DIR}/github/kokoro-github-ojsheikh
+cd ${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-ojsheikh
 ./build.sh
