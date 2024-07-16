d5325414b825dc34c2c2f73a10172ed336dc772e
Omer Sheikh <ojsheikh@gmail.com>
2024-07-16 10:19:32 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ set -x
 
 pwd
 
-git init .
 git config --global --add safe.directory /tmpfs/src/github/kokoro-codelab-ojsheikh
 git status
 
-git config get user.email
-git config get user.name
+git config user.email
+git config user.name
 
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
